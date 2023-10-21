11b0f7aa0126bf1eeae4f7330fc8e31b706a4551
GabrielSaliba <gabriel.saliba.179@gmail.com>
2023-10-21 04:10:23 -0300

    initial settings and background music

---
+++ Assets/Core/Scripts/Settings/MusicVolumeControl.cs
@@ -0,0 +1,48 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicVolumeControl : MonoBehaviour
+{
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private TextMeshProUGUI volumePercentageText;  
+    [SerializeField] private AudioSource backgroundMusic; 
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 0.7f);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+        UpdateText(); 
+    }
+
+    public void ChangeVolume()
+    {
+        backgroundMusic.volume = volumeSlider.value;  
+        Save();
+        UpdateText();  
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        backgroundMusic.volume = volumeSlider.value;  
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+
+    private void UpdateText()
+    {
+        int percentage = Mathf.RoundToInt(volumeSlider.value * 100);
+        volumePercentageText.text = percentage + "%";  
+    }
+}
